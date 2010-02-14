e21781fd8159d822f1aa1f30960c9fccf78d81be
Shim Taewon <marocchino@gmail.com>
2010-02-14 11:14:24 +0900

    8강 끝

---
+++ ScrolliPad/Classes/ScrolliPadViewController.h
@@ -0,0 +1,16 @@
+//
+//  ScrolliPadViewController.h
+//  ScrolliPad
+//
+//  Created by Shim Taewon on 10/02/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScrolliPadViewController : UIViewController {
+	UIImageView *imageView;
+}
+
+@end
+
