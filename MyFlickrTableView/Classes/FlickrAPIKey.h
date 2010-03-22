976d24d01d00bd69c68b79ad35dc1199e3c95b46
Shim Taewon <marocchino@gmail.com>
2010-03-22 09:44:40 +0900

    9강끝

---
+++ MyFlickrTableView/Classes/FlickrAPIKey.h
@@ -0,0 +1,3 @@
+// If you want to build and run this yourself, register for a free Flickr API key and enter it below.
+NSString *const FlickrAPIKey = @"d288b7ceca84b42b0477a3793d25c6c5";
+//b1c5260d194f625b
