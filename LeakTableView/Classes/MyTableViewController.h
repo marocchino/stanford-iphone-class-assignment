d128bcf0eb8c273d93b71e1b48ecd18d607476a2
Shim Taewon <marocchino@gmail.com>
2010-03-22 09:44:48 +0900

    10강 시작

---
+++ LeakTableView/Classes/MyTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  MyTableViewController.h
+//  MyTableView
+//
+//  Created by Evan Doll on 10/21/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MyTableViewController : UITableViewController {
+    NSArray *names;
+}
+
+@end
